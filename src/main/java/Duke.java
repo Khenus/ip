8f8bb2c5ba524f9f73a0bd154b20c959cd93ccab
Khenus <khenus.tan@gmail.com>
2020-08-12 22:18:59 +0800

    V0.0 Level-0: Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
 public class Duke {
+//    public void printer();
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String topPara = "____________________________________________________________\n"
+                        + " Hello! I'm Duke\n"
+                        + " What can I do for you?\n";
+        String midPara = "____________________________________________________________\n";
+        String btmPara = " Bye. Hope to see you again soon!\n"
+                        + "____________________________________________________________";
+
+        System.out.println(topPara + midPara + btmPara);
     }
 }
