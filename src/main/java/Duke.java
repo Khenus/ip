0b8ae518307e204fa9ea3f27146a1975ea13700f
Khenus <khenus.tan@gmail.com>
2020-08-23 01:29:17 +0800

    V0.2 Added Echo and Exit function for Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String paragraph = "____________________________________________________________\n"
-                + " Hello! I'm Duke\n"
-                + " What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________";
+        String content = " Hello! I'm Duke\n"
+                + " What can I do for you?\n";
 
-        System.out.println(paragraph);
+        String topLine = "____________________________________________________________\n";
+        String bottomLine = "____________________________________________________________";
+
+        System.out.println(topLine + content + bottomLine);
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        while (!input.equals("bye")) {
+            if (!input.equals("bye")) {
+                System.out.println(topLine + "You typed in \"" + input + "\"");
+                System.out.println(bottomLine);
+            }
+
+            input = scanner.nextLine();
+        }
+
+        content = " Bye Bye! Hope to see you again soon!\n";
+        System.out.println(topLine + content + bottomLine);
     }
 }
