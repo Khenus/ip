d468341dcfa7b3727347f75f968757329d8b3aa2
Khenus <khenus.tan@gmail.com>
2020-08-23 20:51:40 +0800

    V0.5 Added Add and List function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,34 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+    public static ArrayList<String> allActions = new ArrayList<>();
+
     public static void printWithLines(String stringToPrint) {
         String finalString = "____________________________________________________________\n"
                 + stringToPrint
                 + "\n____________________________________________________________";
 
         System.out.println(finalString);
+    }
+
+    public static void listAllActions() {
+        String fullList = "";
+
+        for (int i = 0; i < allActions.size(); i++) {
+            fullList += " " + (i + 1) + ". " + allActions.get(i);
+
+            if (i != allActions.size() - 1) {
+                fullList +=  "\n";
+            }
+        }
+
+        printWithLines(fullList);
+    }
+
+    public static void addAction(String userInput) {
+        allActions.add(userInput);
+        printWithLines(" added: " + userInput);
     }
 
     public static void main(String[] args) {
@@ -16,7 +38,12 @@ public class Duke {
         String input = scanner.nextLine();
 
         while (!input.equals("bye")) {
-            printWithLines(" You typed in \"" + input + "\"");
+            if (input.equals("list")) {
+                listAllActions();
+            } else {
+                addAction(input);
+            }
+
             input = scanner.nextLine();
         }
 
